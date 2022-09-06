8e69265b6c25c0aee5c5f78d1c07c0ca3fef28cd
zoomshoes <67522195+zoomshoes@users.noreply.github.com>
2022-09-06 10:48:24 -0700

    updated with BOS-SPR trade

--- pullFromJSON.js
+++ pullFromJSON.js
@@ -1,7 +1,4 @@
 import {loadJsonFile} from 'load-json-file';
 //=> {foo: true}
 var exp = await loadJsonFile('New_Exp_League_2023_Fantasy_Draft_8-2.json')
-var players = exp.players
-//console.log(players)
-
-console.log(exp[0].players.pid)
+console.log(loadJsonFile)
